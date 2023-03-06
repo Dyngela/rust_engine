b0de6c640ec8b9d2dd80ad626f74d8119dd1883a
Erwan Nutz <erwan.nutz@free.fr>
2023-03-06 22:57:18 +0100

    adding gl wrapper

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -1,25 +1,9 @@
-#[cfg(test)]
-mod tests
+use env_logger;
+pub use log::*;
+pub mod graphics;
+pub use gl::*;
+
+pub fn init()
 {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    env_logger::init();
 }
-
-pub fn public_function() {
-    println!("Hello I am a public function");
-}
-
-fn private_function() {
-    println!("Hello I am a private function");
-}
-
-pub fn indirect_access() {
-    print!("Accessing a private library using a public function");
-
-    private_function();
-}
-
-pub fn new() {}
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,8 +1,43 @@
 extern crate engine;
 
+use std::{mem, ptr};
+use engine::graphics::gl_wrapper::{BufferObject, Vao, VertexAttribute};
+use engine::graphics::window::Window;
+use engine::types::{GLfloat, GLsizei};
+
 fn main() {
-    println!("Hello, world!");
-    engine::public_function();
-    engine::indirect_access();
-    engine::new()
+    engine::init();
+    let mut window = Window::new(1080, 720, "Hello");
+    window.init_gl();
+
+    let vertices: [f32; 9] = [
+        -0.5, -0.5, 0.0,
+        0.5, -0.5, 0.0,
+        0.0, 0.5, 0.0,
+    ];
+    let vao = Vao::new();
+    vao.bind();
+
+    let vbo = BufferObject::new(engine::ARRAY_BUFFER, engine::STATIC_DRAW);
+    vbo.bind();
+
+    vbo.store_f32_data(&vertices);
+    let position_attribute = VertexAttribute::new(0,
+                                                  3,
+                                                  engine::FLOAT,
+                                                  engine::FALSE,
+                                                  3 * mem::size_of::<GLfloat>() as GLsizei,
+                                                  ptr::null());
+
+    position_attribute.enable();
+
+
+    while !window.should_close() {
+        unsafe {
+            engine::ClearColor(0.3, 0.5, 0.1, 1.0);
+            engine::Clear(engine::COLOR_BUFFER_BIT);
+            engine::DrawArrays(engine::TRIANGLES, 0, 3);
+        }
+        window.update();
+    }
 }
